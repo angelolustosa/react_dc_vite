a233760f4fbb4b7ad2d494e6878470693426b78b
gleidsonteixeira <gleidsondeveloper@gmail.com>
2023-04-17 21:47:02 -0300

    Criando a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import {InputText} from './components/InputText'
+// import "./App.css";
+import { InputText } from "./components/InputText";
+import { MyNavbar } from "./components/MyNavbar";
+import { MyTitle } from "./components/MyTitle";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Olá meu nome é Angelo</h1>
-
-      <InputText />
-      <input type="text" name="nome" id="nome" placeholder="nome"/>
-      <br/>
-      <input type="text" name="sobrenome" id="sobrenome" placeholder="sobrenome"/>
+     <MyNavbar />
     </>
   );
 }
---
+++ src/components/MyTitle.jsx
@@ -0,0 +1,5 @@
+export function MyTitle(props) {
+    return (
+        <h1>{props.title}</h1>
+    )
+}
